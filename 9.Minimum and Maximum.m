7ccd81580a6da19ce1da7d7909cec2465f137d82
Prem Pratap <theprempratap@gmail.com>
2020-05-28 21:59:26 +0530

    min-max in matrix

---
+++ 9.Minimum and Maximum.m
@@ -0,0 +1,25 @@
+%Minimum and Maximum | [Problems](https://www.coursera.org/learn/matlab/gradedLti/NB109/assignment-built-in-functions)
+
+%You have already given calling function
+[mmr, mmm] = minimax([1:4;5:8;9:12])
+
+%below is the sectionn where you have to create function to give exert value.
+
+FUNCTION
+
+function [mmr, mmm] = minimax(A)
+mmr=(max(A,[],2)-min(A,[],2))'
+mmm=max(max(A))-min(min(A))
+
+
+example:
+	>>A = randi(100,3,4)
+	A = 
+		66  94  75  18
+		4   68  40  71
+		85  76  66   4
+	>>[x, y] = minimax(A)
+	x = 
+		76   67   81
+	y = 
+		80
